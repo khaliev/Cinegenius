3547a64510c14942407ecb862f5e751435548085
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-04-12 14:40:11 +0200

    affichage des questions

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,16 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const questionsList = [
+  { id: 1, name: "quel genre de film aimez-vous ?" },
+  { id: 2, name: "deuxieme question" },
+  { id: 3, name: "3 eme question " },
+];
+
+router.get("/questions", (req, res) => {
+  res.json(questionsList);
+});
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const fetchOneTime = () => {
+    fetch("http://localhost:5001/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchedData(data);
+      });
+  };
+
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+      <button type="button" onClick={fetchOneTime}>
+        click me
+      </button>
+      {fetchedData.map((questions) => (
+        <p key={questions.id}>{questions.name}</p>
+      ))}
     </div>
   );
 }
