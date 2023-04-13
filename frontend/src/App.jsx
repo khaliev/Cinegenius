cd83217e08821126f0b46de8125824ae1cd551e5
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-04-13 16:10:33 +0200

    correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,13 @@
-import { useState } from "react";
 import Header from "./components/Header";
+import Questionnaire from "./components/Questionnaire";
 
 import "./App.css";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState([]);
-
-  const fetchOneTime = () => {
-    fetch("http://localhost:5001/questions")
-      .then((response) => response.json())
-      .then((data) => {
-        setFetchedData(data);
-      });
-  };
-
   return (
     <div className="App">
       <Header />
-      <p>coucou</p>
-      <button type="button" onClick={fetchOneTime}>
-        click me
-      </button>
-      {fetchedData.map((questions) => (
-        <p key={questions.id}>{questions.name}</p>
-      ))}
+      <Questionnaire />
     </div>
   );
 }
