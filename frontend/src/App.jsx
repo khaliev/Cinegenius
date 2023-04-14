1e4f5425cd01db4a2fcb429b1803b5528e5a7900
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-04-14 09:38:43 +0200

    changement header ajout du footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Questionnaire from "./components/Questionnaire";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Questionnaire />
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import logo from "../img/logo.png";
+import logo1 from "../img/logo1.png";
+import logo2 from "../img/logo2.png";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <header>
-        <img src={logo} alt="Header" className="logo" />
+        <img src={logo1} alt="Header" className="logo1" />
+        <img src={logo2} alt="Header" className="logo2" />
       </header>
     </div>
   );
