da22024a9da7248b522f86cc3c8349219b58e8fa
MOHAMED MAACH <maach.med@yahoo.com>
2023-04-19 09:48:37 +0200

    adding Footer comp

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <section>n</section>
+    </div>
+  );
+}
+export default Footer;
