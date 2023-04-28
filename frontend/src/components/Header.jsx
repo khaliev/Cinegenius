e25e451549ca60fcb7b9e8b6a453adf7da665ef3
khaliev <dev.adlan@icloud.com>
2023-04-28 14:22:00 +0200

    the logo is now clickable

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,9 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <img src={logo1} alt="Header" className="logo1" />
+      <a href="/">
+        <img src={logo1} alt="Header" className="logo1" />
+      </a>
       <img src={logo2} alt="Header" className="logo2" />
     </header>
   );
